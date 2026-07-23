25e065d46163fb916d87d8b6c5e84b1c8b9321f6
Inderjot Singh <singhinderjot816@gmail.com>
2026-07-23 14:50:22 +0530

    llm

---
+++ Supervisor Agent/llm.py
@@ -0,0 +1,11 @@
+import os 
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+load_dotenv()
+llm=ChatOpenAI(
+    model="openai/gpt-4.1-mini",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1",
+    temperature=0,
+    max_tokens=512
+)
